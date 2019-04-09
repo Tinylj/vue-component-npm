61b4cfb3234083aa0aaa7c17de127df0e360a7e9
lujin <luj@lhcis.com>
2019-04-09 11:18:39 +0800

    打包文件夹

---
+++ src/packages/aside/src/main.vue
@@ -0,0 +1,31 @@
+<template>
+  <aside class="lh-aside" :style="{ width }">
+    <div v-if="showText">
+      测试显示功能
+    </div>
+    <slot></slot>
+  </aside>
+</template>
+
+<script>
+  export default {
+    name: 'LhAside',
+    componentName: 'LhAside',
+    props: {
+      width: {
+        type: String,
+        default: '300px'
+      },
+      showText:{
+        type: Boolean,
+        default:false
+      },
+    }
+  };
+</script>
+<style scoped>
+  .lh-aside {
+    background-color: #3A3F51;
+    height: 1000px;
+  }
+</style>
